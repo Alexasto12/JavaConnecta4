0ef0515efe6bb22216f1fda1abaeb48887bf4115
Alejandro-Cabrera-Carrasco <Alexasto2000@gmail.com>
2024-09-25 21:18:52 +0200

    Modificació de les clases i implementació d'imprimir taulell (encara falta acabar bé)

--- cabreraalejandro_main/src/main/java/connecta4/Game.java
+++ cabreraalejandro_main/src/main/java/connecta4/Game.java
@@ -1,52 +1,65 @@
 package connecta4;
 
 public class Game {
-Player player1;
-Player player2;
-Table main_Table;
+    Player player1;
+    Player player2;
+    Table main_Table;
 
-public Game(Player player1, Player player2, Table main_Table) {
-    this.player1 = player1;
-    this.player2 = player2;
-    this.main_Table = main_Table;
-}
+    public Game(Player player1, Player player2, Table main_Table) {
+        this.player1 = player1;
+        this.player2 = player2;
+        this.main_Table = main_Table;
+    }
 
-public Player getPlayer1() {
-    return player1;
-}
+    public Player getPlayer1() {
+        return player1;
+    }
 
-public void setPlayer1(Player player1) {
-    this.player1 = player1;
-}
+    public void setPlayer1(Player player1) {
+        this.player1 = player1;
+    }
 
-public Player getPlayer2() {
-    return player2;
-}
+    public Player getPlayer2() {
+        return player2;
+    }
 
-public void setPlayer2(Player player2) {
-    this.player2 = player2;
-}
+    public void setPlayer2(Player player2) {
+        this.player2 = player2;
+    }
 
-public Table getMain_Table() {
-    return main_Table;
-}
+    public Table getMain_Table() {
+        return main_Table;
+    }
 
-public void setMain_Table(Table main_Table) {
-    this.main_Table = main_Table;
-}
+    public void setMain_Table(Table main_Table) {
+        this.main_Table = main_Table;
+    }
 
-public boolean remakeGame (String confirmation){
-if (confirmation.toUpperCase() == "Y") {
-    return true;
-} else {
-    return false;
-}
-}
-public void printTable(){
-for (int i = 0; i < array.length; i++) {
-    
-}
+    public boolean remakeGame(String confirmation) {
+        if (confirmation.toUpperCase() == "Y") {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-}
+    public void printTable(char[][] grid) {
+        
+        System.out.println(" " + "--".repeat(grid[0].length));
+        for (int row = 0; row < grid.length; row++) {
+            System.out.print((row + 1) + " |");
+            for (int col = 0; col < grid[0].length; col++) {
+                System.out.print(grid[row][col]);
+                System.out.print("|");
+            }
+            System.out.println();
+            System.out.println("  " +( "--".repeat(grid[0].length)));
+        }
+        System.out.print("  ");
+        for (int i = 1; i <= grid[0].length; i++) {
+            System.out.print(" " + i);
+        }
+        System.out.println();
+    }
 
 }
--- cabreraalejandro_main/src/main/java/connecta4/Table.java
+++ cabreraalejandro_main/src/main/java/connecta4/Table.java
@@ -1,8 +1,10 @@
 package connecta4;
 
+
 public class Table {
 int rows;
 int columns;
+
 
 public Table(int rows, int columns) {
     this.rows = rows;
@@ -24,4 +26,14 @@ public void setColumns(int columns) {
     this.columns = columns;
 }
 
+public char[][] getArray() {
+    char [][] tableArray = new char [this.rows][this.columns];
+    for (int i = 0; i < tableArray.length; i++) {
+        for (int j = 0; j < tableArray[0].length; j++) {
+            tableArray[i][j] = ' ';
+        }
+    }
+    return tableArray;
+
+}
 }
